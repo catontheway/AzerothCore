c29332da9ed7c0345ea8b033698166a363524d9f
IntelligentQuantum <IntelligentQuantum@ProtonMail.Com>
2019-12-27 16:49:44 +0330

    Added Character Check Bag & Work

--- modules/IntelligentQuantum/SQL/World.sql
+++ modules/IntelligentQuantum/SQL/World.sql
@@ -5,3 +5,7 @@ INSERT INTO `creature_template` (`entry`, `modelid1`, `name`, `subname`, `IconNa
 (300003, 12345, '.::Doctor::.', '', 'Speak', 0, 100, 100, 2, 35, 1, 0.8, 0, 2000, 0, 1, 0, 0, 7, 138936390, 100000, 'CreatureScript_Doctor');
 (300004, 12345, '.::Teleport::.', '', 'Speak', 0, 100, 100, 2, 35, 1, 0.8, 0, 2000, 0, 1, 0, 0, 7, 138936390, 100000, 'CreatureScript_Teleport'),
 (300005, 12345, '.::Mall Teleport::.', '', 'Speak', 0, 100, 100, 2, 35, 1, 0.8, 0, 2000, 0, 1, 0, 0, 7, 138936390, 100000, 'CreatureScript_Teleport_Mall');
+
+INSERT INTO `command` VALUES
+('character check bag', 2, 'Syntax: .character check bag [$target_player]\r #bagSlot 1 - 4'),
+('character check work', 2, 'Syntax: .character check work [$target_player]\r\nShow known professions list for selected player');
